f52bd509d6ed6c55fa6c65f70e1e60c25a038202
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2019-12-13 21:58:06 -0700

    Making style changes

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,21 +1,37 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 // import Form from "react-router-dom";
 
 export default function SearchForm({ setSearchTerm }) {
   const handleChange = event => {
     setSearchTerm(event.target.value);
   };
+  const Sect = styled.section`
+    width: 60%;
+    display: flex;
+  `;
+  const Label = styled.label`
+    font-size: 2rem;
+  `;
+  const Form = styled.label`
+    padding-left: 20px;
+  `;
+  const Input = styled.input`
+    width: 100%;
+    height: 20px;
+    margin-bottom: 3px;
+  `;
   return (
-    <section className="search-form">
-      <form>
-        <label htmlFor="search">Search:</label>
-        <input
+    <Sect className="search-form">
+      <Form>
+        <Label htmlFor="search">Search:</Label>
+        <Input
           name="search"
           type="text"
           placeholder="Search characters..."
           onChange={handleChange}
-        ></input>
-      </form>
-    </section>
+        ></Input>
+      </Form>
+    </Sect>
   );
 }
